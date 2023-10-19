537a8cf9bd700937ed6fe1951b4e944b88fd9fb2
Christopher Kaster <me@atomicptr.de>
2023-10-19 21:32:13 +0900

    move ad space to right side if enough space is available

--- src/components/AdSpaceFloating.tsx
+++ src/components/AdSpaceFloating.tsx
@@ -1,27 +1,49 @@
 import { Box, useTheme } from "@mui/material";
 import AdSpace from "@src/components/AdSpace";
 import useIsMobile from "@src/hooks/is-mobile";
+import useWindowSize from "@src/hooks/window-size";
+import React from "react";
+
+export const adSpaceRightSideMinSize = 300;
+export const adSpaceMobileBannerHeight = 96;
 
 const AdSpaceFloating = () => {
-    const isMobile = useIsMobile();
     const theme = useTheme();
+    const isMobile = useIsMobile();
+    const { width } = useWindowSize();
 
-    if (!isMobile) {
+    const rightSideSpace = (width - theme.breakpoints.values.xl) * 0.5;
+    const rightSideHasEnoughSpace = rightSideSpace > adSpaceRightSideMinSize;
+
+    const baseStyle = {
+        background: theme.palette.background.default,
+        display: "flex",
+        position: "fixed",
+    };
+
+    if (!isMobile && !rightSideHasEnoughSpace) {
         return null;
     }
 
-    return (
-        <Box
-            sx={{
-                background: theme.palette.background.default,
+    const style = {
+        ...baseStyle,
+        ...(isMobile
+            ? {
                 bottom: 0,
-                display: "flex",
-                height: "64px",
+                height: `${adSpaceMobileBannerHeight}px`,
                 left: 0,
-                position: "fixed",
                 right: 0,
-            }}
-        >
+            }
+            : {
+                bottom: 0,
+                right: 0,
+                top: 64,
+                width: `${rightSideSpace - adSpaceRightSideMinSize * 0.5}px`,
+            }),
+    };
+
+    return (
+        <Box sx={style}>
             <AdSpace />
         </Box>
     );
