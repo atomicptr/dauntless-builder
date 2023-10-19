c6eab301a4d94b08d9e67a1afe9de81eb6117c90
Christopher Kaster <me@atomicptr.de>
2023-10-19 23:47:48 +0900

    add privacy policy

--- src/pages/about/Privacy.tsx
+++ src/pages/about/Privacy.tsx
@@ -1,21 +1,37 @@
-import { Box, Typography } from "@mui/material";
+import { Warning } from "@mui/icons-material";
+import { Alert, Box, useTheme } from "@mui/material";
+import { styled } from "@mui/material/styles";
 import PageTitle from "@src/components/PageTitle";
 import React from "react";
 import { useTranslation } from "react-i18next";
 
+import privacyContent from "./privacy.html?raw";
+
 const Privacy: React.FC = () => {
     const { t } = useTranslation();
+    const theme = useTheme();
+
+    const PrivacyPoliczWrapper = styled("div")`
+        a {
+            color: ${theme.palette.primary.main};
+            text-decoration: none;
+        }
+    `;
 
     return (
         <Box sx={{ display: "flex", flexDirection: "column", gap: 4, mb: 2 }}>
-            <PageTitle title={t("pages.privacy.title")} />
+            <PageTitle title={t("pages.privacy.title-alt")} />
 
-            <Typography>Lorem ipsum dolor sit amet</Typography>
+            <Alert
+                color="warning"
+                icon={<Warning />}
+            >
+                {t("misc.only-english")}
+            </Alert>
 
-            <PageTitle title={t("pages.privacy.netlify")} />
-            <PageTitle title={t("pages.privacy.cloudflare")} />
-            <PageTitle title={t("pages.privacy.google-analytics")} />
-            <PageTitle title={t("pages.privacy.playwire")} />
+            <PrivacyPoliczWrapper>
+                <div dangerouslySetInnerHTML={{ __html: privacyContent }} />
+            </PrivacyPoliczWrapper>
         </Box>
     );
 };
