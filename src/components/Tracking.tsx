34a95c9ab3bdd2a342248da237c3456dafbc6117
Christopher Kaster <me@atomicptr.de>
2023-10-20 19:33:17 +0900

    remove now unnecessary tracking option as this will be handled by the CMP

--- src/components/Tracking.tsx
+++ src/components/Tracking.tsx
@@ -1,19 +1,10 @@
-import { useAppSelector } from "@src/hooks/redux";
-import { selectConfiguration } from "@src/reducers/configuration/configuration-slice";
 import log from "@src/utils/logger";
 import ReactGA from "react-ga4";
 
 const Tracking = () => {
-    const configuration = useAppSelector(selectConfiguration);
-
     // if user has do not track set, don't do anything
     if (navigator.doNotTrack !== "1") {
         return null;
-    }
-
-    // tracking has not been enabled, skip...
-    if (!configuration.tracking.enabled) {
-        return;
     }
 
     if (DB_GA4_MEASUREMENT_ID) {
