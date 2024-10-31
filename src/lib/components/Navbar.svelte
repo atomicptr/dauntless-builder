068f089264dbddef155dcbe84245acd3d043d575
Christopher Kaster <me@atomicptr.de>
2024-10-31 11:36:16 +0900

    remove account indicator thingy

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -15,12 +15,4 @@ import AppIcon from "$lib/components/AppIcon.svelte";
     </div>
 
     <div class="mx-2 flex-1 hidden lg:block"></div>
-
-    <div class="dropdown dropdown-end">
-        <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
-            <div class="w-10 rounded-full">
-                <img src="https://gravatar.com/avatar/c58aeb397a3ad41836cd14d484c05337" alt="avatar" />
-            </div>
-        </div>
-    </div>
 </div>
