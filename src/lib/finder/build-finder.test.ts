48eccef31a90b8c3008c291b4a6cbeb5295c1cf0
ds-exe <daniel@ds-exe.dev>
2024-11-12 16:14:21 +0000

    Fix generic builds sharing perk data, Add test

--- src/lib/finder/build-finder.test.ts
+++ src/lib/finder/build-finder.test.ts
@@ -1,5 +1,6 @@
 import { expect, inject, test } from "vitest";
 import { findBuildsImplementation } from "./build-finder";
+import { armourStatsForLevel, getCellPerks, mergePerksArray } from "$lib/data/levels";
 
 test("basicBuildFinder1", () => {
     const finderData = inject("finderData");
@@ -48,4 +49,37 @@ test("limitTestLarge", () => {
     expect(findBuildsImplementation(requiredPerks, maxBuilds, finderData, builderData.perks).length).toStrictEqual(
         maxBuilds,
     );
+});
+
+test("validBuildTest", () => {
+    const finderData = inject("finderData");
+    const builderData = inject("builderData");
+
+    const maxBuilds = 50;
+    const requiredPerks = [22, 41, 73];
+    const builds = findBuildsImplementation(requiredPerks, maxBuilds, finderData, builderData.perks);
+    for (const build of builds) {
+        const perkSet = mergePerksArray([
+            builderData.armours[build.head.id]
+                ? (armourStatsForLevel(builderData.armours[build.head.id], build.head.level) ?? {})
+                : {},
+            getCellPerks(build.head.cells),
+            builderData.armours[build.torso.id]
+                ? (armourStatsForLevel(builderData.armours[build.torso.id], build.torso.level) ?? {})
+                : {},
+            getCellPerks(build.torso.cells),
+            builderData.armours[build.arms.id]
+                ? (armourStatsForLevel(builderData.armours[build.arms.id], build.arms.level) ?? {})
+                : {},
+            getCellPerks(build.arms.cells),
+            builderData.armours[build.legs.id]
+                ? (armourStatsForLevel(builderData.armours[build.legs.id], build.legs.level) ?? {})
+                : {},
+            getCellPerks(build.legs.cells),
+        ]);
+        for (const perk of requiredPerks) {
+            const amount = perkSet[perk];
+            expect(amount).toBeGreaterThanOrEqual(builderData.perks[perk].threshold);
+        }
+    }
 });
