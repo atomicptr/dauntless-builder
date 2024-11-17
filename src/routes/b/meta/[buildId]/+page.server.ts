df4be3d6e226c7ce833209547e62ad9ad0dd69c5
Christopher Kaster <me@atomicptr.de>
2024-11-17 17:48:52 +0100

    another attempt at potentially fixing meta page not loading properly

--- src/routes/b/meta/[buildId]/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { fetchBuildsData } from "$lib/data/phalanx";
-import type { PageServerLoad } from "./$types";
-
-export const load: PageServerLoad = async ({ params }) => { 
-    const buildsData = await fetchBuildsData();
-    const build = buildsData.meta.filter((b) => b.id.toString() === params.buildId)[0];
-    return { build };
-};
