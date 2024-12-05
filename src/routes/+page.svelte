f5f4777161cc32a68e70729cb069e3e44bb69e96
Christopher Kaster <me@atomicptr.de>
2024-12-05 17:50:37 +0100

    fix some typos

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,13 +5,13 @@ import { discordLink, githubLink } from "$lib/constants";
 
 <PageTitle title="Home" remove />
 
-<h1 class="text-2xl mb-8 font-bold">Welcome to Dauntless Builder,</h1>
+<h1 class="text-2xl mb-8 font-bold">Welcome to Dauntless Builder</h1>
 
-<p>a free and open source web application for creating and sharing builds for the popular free to play monster hunting game Dauntless.</p>
+<p>A free and open source web application for creating and sharing builds for the popular free to play monster hunting game Dauntless.</p>
 
 <h2 class="text-xl my-4 font-bold">Support</h2>
 
-<p>If you have a problem and/or need help, just jump into our <a class="link-primary" href={discordLink} target="_blank">Discord</a> and ask :).</p>
+<p>If you have a problem and/or need help, just jump into our <a class="link-primary" href={discordLink} target="_blank">Discord</a> and ask :)</p>
 
 <h2 class="text-xl my-4 font-bold">Contributing</h2>
 
