631ac958a2808ecdc2a15013a88eb6783cc3d923
ds-exe <daniel@ds-exe.dev>
2024-12-05 00:34:56 +0000

    Fix talent stat text

--- src/lib/components/TalentOptionText.svelte
+++ src/lib/components/TalentOptionText.svelte
@@ -12,15 +12,15 @@ const { option, compact, ...rest }: Props = $props();
 </script>
 
 <div class={rest.class}>
-    {#if option.type === 'custom'}
+    {#if option.type === "custom"}
         <ValuesText
             text={option.description}
             values={option.values}
-            classOverwrite={compact ? {p: ""} : undefined}
+            classOverwrite={compact ? { p: "" } : undefined}
         />
-    {:else if option.type === 'stat'}
+    {:else if option.type === "stat"}
         <div>
-            You have {option.value} additional point held of {option.stat.toUpperCase()}
+            You have {option.value} additional point of held {option.stat.toUpperCase()}.
         </div>
     {/if}
 </div>
