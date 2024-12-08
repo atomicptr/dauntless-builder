ee15d7815c7aca0cd2d690c6ccc8c6745084ca8c
Christopher Kaster <me@atomicptr.de>
2024-12-08 17:02:03 +0100

    improve static page handling

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,7 @@ const config = {
     preprocess: vitePreprocess(),
 
     kit: {
-        adapter: adapter({ fallback: "index.html" }),
+        adapter: adapter({ fallback: "app.html" }),
         paths: {
             relative: false,
         },
