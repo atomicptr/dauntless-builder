e0093992e4c1188dfa64c724efe05551db28b910
Christopher Kaster <me@atomicptr.de>
2024-12-08 16:26:49 +0100

    filter out invalid items from finder options

--- src/routes/b/finder/[options]/+page.ts
+++ src/routes/b/finder/[options]/+page.ts
@@ -1,6 +1,16 @@
 import { finderPageDataDeserialize } from "$lib/finder/initial";
 import type { PageLoad } from "./$types";
 
-export const load: PageLoad = ({ params }) => {
-    return { finderPageData: finderPageDataDeserialize(params.options) };
+export const load: PageLoad = async ({ parent, params }) => {
+    const data = await parent();
+    const finderPageData = finderPageDataDeserialize(params.options);
+
+    // filter out invalid items
+    finderPageData.perks = finderPageData.perks.filter((perkId) => perkId in data.perks);
+    finderPageData.items.heads = finderPageData.items.heads.filter((armourId) => armourId in data.armours);
+    finderPageData.items.torsos = finderPageData.items.torsos.filter((armourId) => armourId in data.armours);
+    finderPageData.items.arms = finderPageData.items.arms.filter((armourId) => armourId in data.armours);
+    finderPageData.items.legs = finderPageData.items.legs.filter((armourId) => armourId in data.armours);
+
+    return { finderPageData };
 };
