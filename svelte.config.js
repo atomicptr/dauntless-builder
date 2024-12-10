8a38171bc2e1a947f35c456146150d3b68430210
Christopher Kaster <me@atomicptr.de>
2024-12-10 16:01:49 +0100

    switch to adapter-cloudflare, turn on ssr for builder and finder (#40)

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-cloudflare";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,7 +6,7 @@ const config = {
     preprocess: vitePreprocess(),
 
     kit: {
-        adapter: adapter({ fallback: "index.html" }),
+        adapter: adapter(),
         paths: {
             relative: false,
         },
