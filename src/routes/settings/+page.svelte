c76760b6537d75204b925848a7ff315bd3d621ef
Christopher Kaster <me@atomicptr.de>
2024-12-11 14:07:25 +0100

    add view weapon abilities/talents options to settings

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 import PageTitle from "$lib/components/PageTitle.svelte";
-import { theme } from "$lib/state.svelte";
+import { configViewWeaponAbilities, configViewWeaponTalents, theme } from "$lib/state.svelte";
 
 const onThemeChanged = () => {
     theme.set($theme === "dark" ? "light" : "dark");
@@ -15,6 +15,20 @@ const onThemeChanged = () => {
         <label class="label cursor-pointer">
             <span class="label-text">Use Dark Mode</span>
             <input type="checkbox" class="toggle toggle-primary" checked={$theme === "dark"} onchange={onThemeChanged} />
+        </label>
+    </div>
+
+    <div class="form-control max-w-64">
+        <label class="label cursor-pointer">
+            <span class="label-text">View Weapon Abilities</span>
+            <input type="checkbox" class="toggle toggle-primary" checked={$configViewWeaponAbilities} onchange={() => configViewWeaponAbilities.set(!$configViewWeaponAbilities)} />
+        </label>
+    </div>
+
+    <div class="form-control max-w-64">
+        <label class="label cursor-pointer">
+            <span class="label-text">View Weapon Talents</span>
+            <input type="checkbox" class="toggle toggle-primary" checked={$configViewWeaponTalents} onchange={() => configViewWeaponTalents.set(!$configViewWeaponTalents)} />
         </label>
     </div>
 </div>
